5d2da8720f5ad948102bc4583975f73862d84559
teacherHakkieva <teacher.hakkieva@gmail.com>
2026-06-30 22:40:48 +0300

    Add Preson class

---
+++ DemoProject/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    internal class Person
+    {
+        string name { get; set; }
+        int age { get; set; }
+        double salary;
+
+        public Person() { }
+
+        public double Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+         public  void PresetMe()
+        {
+            Console.WriteLine($"Name:{name}-age:{age} and salary:{this.salary}");
+        }
+
+    }
+}
