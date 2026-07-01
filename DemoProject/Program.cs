7f98e2896b687b093a1bfe5c3ce4de0f51689030
Teacher <teacher.hakkieva@gmail.com>
2026-07-01 23:25:10 +0300

    Cread in main CRUD choose

--- DemoProject/Program.cs
+++ DemoProject/Program.cs
@@ -5,7 +5,25 @@
         static void Main(string[] args)
         {
             Person mitak = new Person("Ivan",15,1234.56);
-            mitak.PresetMe();
+            Console.WriteLine(mitak.ToString());
+
+            List<Person> people = new List<Person>();
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("---Избери операция---");
+                Console.WriteLine("1. CREATE (Добавяне на човек)");
+                Console.WriteLine("2. READ (Показване на всички)");
+                Console.WriteLine("3. UPDATE (Промяна на заплата)");
+                Console.WriteLine("4. DELETE (Изтриване по име)");
+                Console.WriteLine("5. Изход");
+                Console.Write("Избор: ");
+
+                break   ;
+
+
+            }
         }
     }
 }
